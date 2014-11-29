7fd30501aca01daa5efa973c272ed0b68edd821b
diazdavid-info <diazdavid.info@gmail.com>
2014-11-29 19:31:36 +0100

    Signed-off-by: diazdavid-info <diazdavid.info@gmail.com>

--- createForm/libraryCreateForm/ExceptionForm.php
+++ createForm/libraryCreateForm/ExceptionForm.php
@@ -1,38 +1,59 @@
 <?php
+/**
+ * 
+ * @author David Díaz
+ * @version 1.0
+ *
+ */
 
 namespace createForm\form;
 
 class ExceptionForm extends \Exception{
 	
-	private $attrForm = array('accept-charset','action','autocomplete','enctype','method','name','novalidate','target','accept','id');
-	private $tagForm = array('input');
-	private $attrInput = array('accept','after','alt','autocomplete','autofocus','before','checked','class','dirname','disabled','form','formaction','formenctype','formmethod','formnovalidate',
-			'formtarget','height','id','inputmode','list','max','maxlength','min','minlength','multiple','name','pattern','placeholder','readonly','required','size','src','step',
-			'type','value','width','title');
-	
+	/**
+	 * Constructor reservado para posibles mejoras.
+	 */
 	function __construct(){
 		
 	}
 	
+	/**
+	 * Función que ckequea si los atributos que tiene la etiqueta <form> son correctos.
+	 * Si la función detecta que la etiqueta <form> tiene un atributo no válido la función levanta una excepción.
+	 * @param array $attributes Array con los atributos que se desean añadir a la etiqueta <form>.
+	 * @throws \Exception
+	 */
 	public function checkForm(array $attributes){
 		foreach ($attributes as $key => $vale){
-			if(!in_array($key, $this->attrForm)){
+			if(!in_array($key, Form::getAttrForm())){
 				throw new \Exception("No se reconoce {$key} como atributo de la etiqueta form");
 			}
 		}
 	}
 	
+	/**
+	 * Función que ckequea que las etiquetas que se van a encerrar dentro de la etiqueta <form> son propias de un formulario.
+	 * Si la función detecta que se quiere añadir una etiqueta que no corresponden con las etiquetas típicas de un formulario levanta una excepción.
+	 * @param array $body Array con las etiquetas que se van a añadir dentro de la etiqueta <form>.
+	 * @throws \Exception
+	 */
 	public function checkBody(array $body){
 		foreach ($body as $key => $vale){
-			if(!in_array($key, $this->tagForm)){
+			if(!in_array($key, Form::getTagForm())){
 				throw new \Exception("No se reconoce {$key} como etiqueta para un formulario");
 			}
 		}
 	}
 	
+	/**
+	 * Función que chekea que los atributos que se intentan añadir dentro de la etiqueta <input> son correctos.
+	 * Si la función detecta que se intenta añadir atributos que no son validos para la etiqueta <input> levanta una excepción.
+	 * @param array $inputAttr Array con los atributos que se van a añadir a la etiqueta <input>.
+	 * @throws \Exception
+	 */
 	public function checkInput(array $inputAttr){
 		foreach ($inputAttr as $key => $vale){
-			if(!in_array($key, $this->attrInput)){
+			if(!in_array($key, Form::getAttrInput())){
 				throw new \Exception("No se reconoce {$key} como atributo de la etiqueta input");
 			}
 		}
--- createForm/libraryCreateForm/Form.php
+++ createForm/libraryCreateForm/Form.php
@@ -3,7 +3,24 @@
 namespace createForm\form;
 
 class Form{
+	private static $attrForm = array('accept-charset','action','autocomplete','enctype','method','name','novalidate','target','accept','id');
+	private static $tagForm = array('input');
+	private static $attrInput = array('accept','after','alt','autocomplete','autofocus','before','checked','class','dirname','disabled','form','formaction','formenctype','formmethod','formnovalidate',
+			'formtarget','height','id','inputmode','list','max','maxlength','min','minlength','multiple','name','pattern','placeholder','readonly','required','size','src','step',
+			'type','value','width','title');
 	private static $attrPrivate = array('after','before');
+	
+	public static function getAttrForm(){
+		return self::$attrForm;
+	}
+	
+	public static function getTagForm(){
+		return self::$tagForm;
+	}
+	
+	public static function getAttrInput(){
+		return self::$attrInput;
+	}
 	
 	public static function getAttrPrivate(){
 		return self::$attrPrivate;
